66f37c8963e727f1a492d08747c65d931a3d7d05
Victor Rodriguez <viictor.axel11@gmail.com>
2024-10-20 20:01:39 -0600

    subir

--- prueba.py
+++ prueba.py
@@ -6,15 +6,17 @@ vis.create_window(window_name="Open3D", width=1920, height=1080)
 vis.add_geometry(mesh)  # Añade el modelo 3D a la ventana de visualización.
 vis.get_render_option().load_from_json("render_options.json")  # Carga las opciones de renderización desde un archivo JSON.
 vis.get_view_control().set_zoom(0.7)  # Establece el nivel de zoom de la vista.
-vis.get_view_control().rotate(300, 1000, xo=0.0, yo=0.0)  # Rota la vista del modelo 3D.
-vis.get_view_control().rotate(1000, 0, xo=0.0, yo=0.0)  # Rota la vista del modelo 3D.
 vis.poll_events()  # Procesa los eventos de la ventana.
 vis.update_renderer()  # Actualiza el renderizador.
 
 vis.poll_events()  # Procesa los eventos de la ventana.
 vis.update_renderer()  # Actualiza el renderizador.
 
-vis.run()
+while True:
+    ctrl = vis.get_view_control()
+    ctrl.rotate(3, 0, xo=0.0, yo=0.0)
+    vis.poll_events()  # Procesa los eventos de la ventana.
+    vis.update_renderer()  # Actualiza el renderizador.
 
 # Cierra y destruye la ventana una vez el usuario la cierre
 vis.destroy_window()
